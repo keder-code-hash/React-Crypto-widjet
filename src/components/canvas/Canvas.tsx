974e1704a5f5b2a7b65d5ca960014b834a5e7fc7
Keder Nath mallick <53966650+keder-code-hash@users.noreply.github.com>
2022-10-26 12:49:53 +0530

    map added

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -2,31 +2,22 @@ import React, { useEffect, useRef } from 'react'
 import { plotGraph,_drawLine } from '../plot-graph/chart-plot';
 import "./Canvas.css";
 
-async function fetchData(coin_name:string,day:number,period:string){
-  const URL="https://api.coingecko.com/api/v3/coins/"+coin_name+"/market_chart?vs_currency=USD&days="+day+"&interval="+period;
-  let response=await fetch(URL);
-  const data=await response.json();
-  let priceArray=data.prices;
-  let prices=[]
-  for(let i=0;i<priceArray.length;i++){
-      prices.push(priceArray[i][1]);
-  }
-  return prices;
+type coinData={
+  data:number[];
 }
 
-
-export const Canvas : React.FC<{}> = () => {
-  
-    const canvasRef = useRef<HTMLCanvasElement | null>(null);
-    useEffect(()=>
-    {
-      const canvas = canvasRef.current;
-      if(canvas!=null){
-          // let data:number[]=[1,2,3,4,5,6,7,8];
+export const Canvas = (props:coinData) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  useEffect(()=>
+  {
+    const canvas = canvasRef.current;
+    if(canvas!=null){
+          console.log(props.data);
           const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-          fetchData("bitcoin",7,"hourly").then((data)=>plotGraph(canvas,context,data));
+          context.clearRect(0, 0, canvas.width, canvas.height);
+          plotGraph(canvas,context,props.data);
       }
-    },[]);
+    },[props]);
     
   return( 
     <canvas id="canvas" height="400" width="600" ref={canvasRef}/>
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,31 +1,72 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react'
 import './home.css';
-import {Canvas} from "../../components/canvas/Canvas"
+import {Canvas} from "../../components/canvas/Canvas";
 
-function handleClickPeriod():void{
 
-}
-function handleClickCoins():void{
+async function fetchData(coin_name:string,day:number,period:string){
+    const URL="https://api.coingecko.com/api/v3/coins/"+coin_name+"/market_chart?vs_currency=USD&days="+day+"&interval="+period;
+    let response=await fetch(URL);
+    const data=await response.json();
+    let priceArray=data.prices;
+    let prices=[]
+    for(let i=0;i<priceArray.length;i++){
+        prices.push(priceArray[i][1]);
+    }
+    return prices;
+  }
+  
 
-}
+
 export function Home(){
+    const [data,setData]=React.useState<number[]>([]);
+    const [period,setPeriod]=React.useState<number>(1);
+    const [coin,setCoin]=React.useState<string>("bitcoin");
+
+    const handleClickPeriod=(e:any)=>{
+        let innerText:string=e.target.innerText;
+        if(innerText==='1D'){
+            setPeriod(1);
+        }
+        else if(innerText=='7D'){
+            setPeriod(7);
+        }
+        else if(innerText=='30D'){
+            setPeriod(30);
+        }
+        fetchData(coin,period,"hourly").then((data)=>setData(data));
+    } 
+    const handleClickCoins=(e:any)=>{
+        let innerText:string=e.target.innerText;
+        if(innerText==='ETH'){
+            setCoin("ethereum");
+        }
+        else if(innerText=='BTC'){
+            setCoin("bitcoin");
+        }
+        fetchData(coin,period,"hourly").then((data)=>setData(data));
+    }
+
+    useEffect(()=>{
+        fetchData(coin,period,"hourly").then((data)=>setData(data));
+    },[]);
+
     return(
         <React.Fragment>
             <div className="container">
                 <div className="row">
                     <div className="col col-sm-12 col-md-12 col-lg-12">
-                        <Canvas/>
+                        <Canvas data={data}/>
                     </div>
                 </div>
                 <div className="row"> 
                     <div className="btn-group">
-                        <button id='period-btn' onClick={handleClickPeriod}>1D</button>
-                        <button id='period-btn' onClick={handleClickPeriod}>7D</button>
-                        <button id='period-btn' onClick={handleClickPeriod}>30D</button>
+                        <button id='period-btn-1D' className='period-btn' onClick={handleClickPeriod}>1D</button>
+                        <button id='period-btn-7D' className='period-btn' onClick={handleClickPeriod}>7D</button>
+                        <button id='period-btn-30D' className='period-btn' onClick={handleClickPeriod}>30D</button>
                     </div>
                     <div className="btn-group">
-                        <button id='period-btn' onClick={handleClickCoins}>BTC</button>
-                        <button id='period-btn' onClick={handleClickCoins}>ETH</button> 
+                        <button id='period-btn-btc' className='period-btn' onClick={handleClickCoins}>BTC</button>
+                        <button id='period-btn-eth' className='period-btn' onClick={handleClickCoins}>ETH</button> 
                     </div>
                 </div>
             </div>
