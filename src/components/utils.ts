30275488bb363b9f602b9e5dac19b9c2dd5ab82f
Keder Nath mallick <53966650+keder-code-hash@users.noreply.github.com>
2022-10-27 00:53:08 +0530

    cleaning up

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,8 +1,14 @@
+/**
+ * 
+ * @param coin_name : Name of the Coin.
+ * @param day : Day for data to be fetched - 1,7,30;
+ * @param period : Period for data to be fetched - hourly,minutely;
+ * @returns : Fetched price list for mentioned data.
+ */
 export async function fetchData(coin_name:string,day:number,period:string){
     const URL="https://api.coingecko.com/api/v3/coins/"+coin_name+"/market_chart?vs_currency=USD&days="+day+"&interval="+period;
     let response=await fetch(URL);
     const data=await response.json();
-    const status=await response.status;
     let priceArray=data.prices;
     let prices=[]
     for(let i=0;i<priceArray.length;i++){
